dded213666ff5fb451bd7f1b7414eb9c87fe5b28
satoshi07 <satoshi.akaike7@gmail.com>
2021-07-11 13:32:21 +0900

    初期データの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,21 @@
-# テーブルを再生成
-ActiveRecord::Base.connection.execute('TRUNCATE TABLE users RESTART IDENTITY CASCADE')
-ActiveRecord::Base.connection.execute('TRUNCATE TABLE posts RESTART IDENTITY CASCADE')
+email = 'test@example.com'
+password = 'password'
 
+# users テーブルを再生成（関連付いたテーブルを含む）
+ActiveRecord::Base.connection.execute('TRUNCATE TABLE users RESTART IDENTITY CASCADE')
+
+# 以下，開発用の初期データ
 user1 = User.create!(email: 'satou@example.com', password: 'password')
 user2 = User.create!(email: 'suzuki@example.com', password: 'password')
 user3 = User.create!(email: 'tanaka@example.com', password: 'password')
 
-Post.create!(content: 'おはよう', user_id: user2.id)
-Post.create!(content: 'こんにちは', user_id: user3.id)
-Post.create!(content: 'こんばんは', user_id: user3.id)
+user2.posts.create!(content: '沖縄そば')
+user1.posts.create!(content: '海ぶどう')
+user3.posts.create!(content: 'ゴーヤーチャンプルー')
+user3.posts.create!(content: 'サーターアンダギー')
+user1.posts.create!(content: 'タコライス')
+
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: email, password: password)
 
 puts '初期データの投入に成功しました！'
